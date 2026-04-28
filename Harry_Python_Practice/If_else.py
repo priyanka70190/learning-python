06ae1fe3ff6e1db02629b8189c09460a5f5824d5
priyanka70190 <priyankaarora70190@gmail.com>
2026-04-28 12:53:43 +0530

    Create If_else.py

---
+++ Harry_Python_Practice/If_else.py
@@ -0,0 +1,19 @@
+"""var1=6
+var2=56
+
+var3=int(input())
+
+if (var3>var2):
+    print("var3 is greater than var2")
+elif(var3==var2):
+     print("var3 is equal to vr2")
+else:
+    print("variable3 is less than var2")"""
+
+list1=[5,7,3]
+if 5 in list1:
+    print("yes,5 is in list1")
+if 15 not in list1:
+    print("no 15 it is not in the list")
+
+#ask user to enter name
