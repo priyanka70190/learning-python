f2913fe53b6441dcf44f1eb74c01840b169b3f4a
priyanka70190 <priyankaarora70190@gmail.com>
2026-05-16 08:07:49 +0530

    Create student_profile.py

---
+++ Assignment8/student_profile.py
@@ -0,0 +1,8 @@
+"""Ask the user to enter their name,age and city.Use an F-string to print a formatted intro sentence"""
+print("Enter your name:")
+name=input()
+print("Enter your age:")
+age=input()
+print("Enter your city:")
+city=input()
+print(f"Hi! My name is {name} , I am {age} years old and I live in {city}")
