b03bd834d0d1bdc689b3ea59d91e6ba2ad60a1f3
priyanka70190 <priyankaarora70190@gmail.com>
2026-06-13 22:09:45 +0530

    Create BankAccount.py

---
+++ Assignment11/BankAccount.py
@@ -0,0 +1,26 @@
+"""
+PROBLEM STATEMENT:
+Define a class called BankAccount.
+Add an attribute balance.
+Add a function : deposit(amount) to add money,
+Add another function: withdraw(amount) to deduct money.
+If the withdrawal amount is greater than the balance, raise a custom error InsufficientFundsError.
+Print the balance after each operation"""
+
+class BankAccount:
+    def __init__(self,balance):
+        self.balance = balance
+    def deposit(self,amount):
+        self.balance += amount
+        print("Balance after deposit is:",self.balance)
+    def withdraw(self,amount):
+        if amount > self.balance:
+            print("Insufficient Funds")
+        else:
+          self.balance -= amount
+          print("Balance after withdraw is:",self.balance)
+
+obj = BankAccount(0)
+obj.deposit(1000)
+obj.withdraw(400)
+
